0b80456dab9875404d4ecdc8b67a74ed8f24a520
Lokesh Redekar <redekarlk123@gmail.com>
2026-07-07 15:44:55 +0530

    wired routes into express application entry point

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import express from 'express';
 import morgan from 'morgan';
 import { connectDB } from './config/db.js';
 
+import askRouter from './routes/ask.js';
+import contradictRouter from './routes/contradict.js';
+import ingestRouter from './routes/ingest.js';
+import documentsRouter from './routes/documents.js';
 
 dotenv.config();
 
@@ -14,14 +18,22 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-connectDB(); //calling database
+connectDB();
 
 app.get('/', (req, res) => {
-	res.json({
-		message: 'Backend is running',
-	});
+	res.json({ message: 'Backend is running' });
 });
 
+app.use('/ask', askRouter);
+app.use('/contradict', contradictRouter);
+app.use('/ingest', ingestRouter);
+app.use('/documents', documentsRouter);
+
+// error handler
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).json({ error: 'Something went wrong' });
+});
 
 app.listen(port, () => {
 	console.log(`Server running on ${port}`);
